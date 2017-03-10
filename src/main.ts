7ecc8566a5b16626bc817ee371169dc9c6c17e82
compscidude <grahammeuller@hotmail.com>
2017-03-10 23:14:17 +0900

    finalized boiler plate

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import 'core-js'
+import 'reflect-metadata'
+import 'zone.js/dist/zone'
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule }              from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+// the main.ts finally builds everything together.
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+
+
+// This is a node module
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    entry: './src/main.ts',
+    output : {
+        path: './dist/',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.ts$/, loader: 'ts'}
+        ]
+    },
+    resolve: {
+        extensions : ['', '.js', '.ts']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
+
+
+
